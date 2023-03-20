e8f3e965ba183fe6998a363e483da8ba51bbe91b
Jhonny Bro <103278130+JhonnyB0098@users.noreply.github.com>
2023-03-20 17:38:14 +0000

    initial commit

---
+++ 004_operators.js
@@ -0,0 +1,45 @@
+//Arithmetic Operators
+let a=45
+let b=4
+console.log("a + b = ",a+b)
+console.log("a - b = ",a-b)
+console.log("a * b = ",a*b)
+console.log("a / b = ",a/b)
+console.log("a ^ b = ",a**b)
+console.log("a % b = ",a%b)
+console.log("a++= ",a++)
+console.log("++a = ",++a)
+console.log("a-- = ",a--)
+console.log("--a = ",--a)
+
+//Assignment Operators
+let c=1
+c+=5 //same as c=c+5
+console.log(c)
+c-=5 //same as c=c-5
+console.log(c)
+c*=5 //same as c=c*5
+console.log(c)
+c/=5 //same as c=c/5
+console.log(c)
+c**=2 //same as c^2
+console.log(c)
+
+// Comparison Operators
+let d=6
+let e=7
+let f="7"
+console.log("d==e is",d==e)
+console.log("d===e is",d===f)
+console.log("d!=e is",d!=e)
+console.log("d!==e is",d!==f)
+console.log("d>e is",d>e)
+console.log("d<e is",d<e)
+
+//Logical Operators
+let x=5
+let y=6
+console.log(x<y && x==5)
+console.log(x>y || y==6)
+console.log(!false)
+console.log(!true)
