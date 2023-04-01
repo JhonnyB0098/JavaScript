a8498b6a22de2a668f1390ea51ae2872aba394af
Jhonny Bro <103278130+JhonnyB0098@users.noreply.github.com>
2023-04-01 17:51:37 +0000

    initial commit

---
+++ 005_conditional_expression.js
@@ -0,0 +1,8 @@
+let a=prompt("Whats your age")
+a=Number.parseInt(a)  //Converting the string to a number
+if(a>0){
+    alert("This is a valid age")
+}
+else{
+    alert("This is an Invalid age")
+}
